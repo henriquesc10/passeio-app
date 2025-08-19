2fb1f706e726d51790fae9ce1f02c73c264ad2a1
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-08-19 10:14:37 -0300

    feat: adiciona tela de galeria

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Lugar } from '../../lugares/lugar';
+import { Categoria } from '../../categorias/categoria';
+import { LugarService } from '../../lugares/lugar.service';
+import { CategoriaService } from '../../categorias/categoria.service';
 
 @Component({
   selector: 'app-galeria',
@@ -6,6 +10,21 @@ import { Component } from '@angular/core';
   templateUrl: './galeria.component.html',
   styleUrl: './galeria.component.scss'
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements OnInit {
 
+  lugares: Lugar[] = [];
+  categoriasFiltros: Categoria[] = [];
+
+  constructor(
+    private lugarService: LugarService,
+    private categoriaService: CategoriaService
+  ) { }
+
+  ngOnInit(): void {
+    this.categoriaService.obterTodas()
+      .subscribe(categoriasResultado => this.categoriasFiltros = categoriasResultado);
+
+    this.lugarService.obterTodos()
+      .subscribe(lugaresResultado => this.lugares = lugaresResultado);
+  }
 }
