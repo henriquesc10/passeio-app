3e9e97b936db88bf54fca34dce720d83c7a55ba1
Henrique Silva Camargo <henrique.camargo@serpro.gov.br>
2025-08-19 10:55:35 -0300

    feat: adiciona função de filtro por categoria

--- src/app/galeria/galeria/galeria.component.ts
+++ src/app/galeria/galeria/galeria.component.ts
@@ -13,6 +13,8 @@ import { CategoriaService } from '../../categorias/categoria.service';
 export class GaleriaComponent implements OnInit {
   lugares: Lugar[] = [];
   categoriasFiltros: Categoria[] = [];
+  nomeFiltro: string = '';
+  categoriaFiltro: string = '';
 
   constructor(
     private lugarService: LugarService,
@@ -32,6 +34,16 @@ export class GaleriaComponent implements OnInit {
   }
 
   getTotalEstrelas(lugar: Lugar): string {
-    return '&#9733'.repeat(lugar.avaliacao || 0) + '&#9734'.repeat(5 - (lugar.avaliacao || 0));
+    return (
+      '&#9733'.repeat(lugar.avaliacao || 0) +
+      '&#9734'.repeat(5 - (lugar.avaliacao || 0))
+    );
+  }
+
+  filtrar() {
+    console.log('entrou em filtrar', this.nomeFiltro, this.categoriaFiltro);
+    this.lugarService
+      .filtrar(this.nomeFiltro, this.categoriaFiltro)
+      .subscribe((resultado) => (this.lugares = resultado));
   }
 }
--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -1,14 +1,13 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Lugar } from './lugar';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LugarService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   salvar(lugar: Lugar): Observable<Lugar> {
     return this.http.post<Lugar>('http://localhost:3000/lugares', lugar);
@@ -16,5 +15,21 @@ export class LugarService {
 
   obterTodos(): Observable<Lugar[]> {
     return this.http.get<Lugar[]>('http://localhost:3000/lugares');
+  }
+
+  filtrar(nome: string, categoria: string): Observable<Lugar[]> {
+    let parametros = new HttpParams();
+
+    if (nome) {
+      parametros = parametros.set('nome_like', nome);
+    }
+
+    if (categoria) {
+      parametros = parametros.set('categoria', categoria);
+    }
+
+    return this.http.get<Lugar[]>('http://localhost:3000/lugares', {
+      params: parametros,
+    });
   }
 }
